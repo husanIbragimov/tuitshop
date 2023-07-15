cf3b14e42ad1f0b43933237ac514cd94b18de434
husanibragimov <ibragimovdev@tstu.uz>
2023-07-15 13:03:50 +0500

    set database 2

--- core/settings.py
+++ core/settings.py
@@ -109,24 +109,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # ================================================== PostgresSQL =======================================================
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': os.environ.get('NAME'),
-#          'USER': os.environ.get('DBUSER'),
-#          'PASSWORD': os.environ.get('PASSWORD'),
-#          'HOST': 'localhost',
-#          'PORT': 5432,
-#      }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': os.environ.get('NAME'),
+         'USER': os.environ.get('DBUSER'),
+         'PASSWORD': os.environ.get('PASSWORD'),
+         'HOST': 'localhost',
+         'PORT': 5432,
+     }
+}
 
 
 # Password validation
